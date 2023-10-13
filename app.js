742db8a7acce5b4a50ee0e8b04a4ddfa8d54be6d
LeonKingK <kaunglwinmyo17klm@gmail.com>
2023-10-13 09:53:16 +0630

    Gallery

--- app.js
+++ app.js
@@ -1,27 +1,75 @@
 require('dotenv').config();
+
+// const patch=require('patch');
+
 const express=require('express');
 const app=express();
+const fileUpload=require('express-fileupload');
 
+//database connecting
+const mongoose=require('mongoose');
+mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`);
 
 app.use(express.json());
+app.use(fileUpload());
+
+// app.use('/uploads',express.static(patch.join(__dirname,'uploads')));
 
 let userRoute=require('./routes/user');
 let postRoute=require('./routes/post');
 
-const mongoose=require('mongoose');
+const {saveFile,saveFiles,deleteFile}=require('./utils/gallery');
 
-mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`);
+app.post('/gallery',async (req,res,next)=>{
+    await deleteFile(req.body.name);
+    res.status(200).json({msg:"File Deleted"});
+    // res.status(200).json({msg:"File Uploaded", "filenames":req.body.images});
+});
 
+
+app.post('/galleryy', saveFiles , (req,res,next)=>{
+    res.status(200).json({msg:"File Uploaded", "filenames":req.body.images});
+});
+
+// const funky=(req,res,next)=>{
+//     console.log(req.warningMsg);
+//     res.json({msg:"coming with get method"});
+// };
+
+// const islogged=(req,res,next)=>{
+//     if(1+1==2){
+//         req.successMsg="We are good to go";
+//         next();
+//     }else{
+//         next(new Error("You are not logged in"));
+//     }
+// }
+
+// const isAdmin=(req,res,next)=>{
+//     if(3+3 ==6){
+//         console.log(req.successMsg);
+//         req.warningMsg="this is warning message";
+//         next();
+//     }else{
+//         next(new Error("Only admin can acess this route"));
+//     }
+// }
+
+// app.get('/users',islogged,isAdmin,funky);
+
+// app.use('/users',userRoute);
 app.use('/users',userRoute);
 app.use('/posts',postRoute);
 
+//error handling
 app.use((err,req,res,next)=>{
     err.status=err.status || 200;
     res.status(err.status).json({
         cons:false,
         msg:err.message
-    })
+    });
 
-})
+});
 
 app.listen(process.env.PORT,console.log(`Server is running at port ${process.env.PORT}`));
+
--- controllers/post.js
+++ controllers/post.js
@@ -1,15 +1,27 @@
-const all=async(req,res,next)=>{
-    res.json({msg:"All users"});
-    };
+    const DB=require('../dbs/post');
+    const Helper = require('../utils/helper');
     
-    const post=async(req,res,next)=>{
-        res.json({msg:"All user posts is here",result:req.body});
+
+    const all=async(req,res,next)=>{
+    let posts= await DB.find().populate("user","-password -__v");
+    Helper.fMsg(res, "All posts", posts);
     };
     
     const get=async(req,res,next)=>{
-        res.json({msg:"All get is here"});
+        let post= await DB.findById(req.params.id).populate("user");
+        if(post){
+            Helper.fMsg(res, "Single Post" , post);
+        }else{
+            next(new Error("No Post with that id sir"));
+        }
+    };
+
+    const post=async(req,res,next)=>{
+        let result=await new DB(req.body).save();
+        Helper.fMsg(res,"Post Added", result);
     };
     
+   
     const patch=async( req ,res ,next)=>{
         res.json({msg:"All user patch is here"});
     };
